6ce08bfd4be9eecf14276bf331fdab6e3a4ef625
Daniel Arantes Telles <daniel.telles197@gmail.com>
2022-01-10 20:11:51 -0300

    System_Prop: Dalvik Heap Update

--- system_prop.mk
+++ system_prop.mk
@@ -17,10 +17,10 @@
 # Dalvik Heap
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
 
 # Audio
